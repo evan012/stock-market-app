75ff4f2a149885a8a5179d46a4aa6e3869bcd476
Evan Xu <xuevan12@gmail.com>
2019-04-10 23:46:51 -0700

    updated UI

--- app/src/main/java/org/pltw/examples/stocksearchapp/CryptoSearch.java
+++ app/src/main/java/org/pltw/examples/stocksearchapp/CryptoSearch.java
@@ -1,20 +1,23 @@
 package org.pltw.examples.stocksearchapp;
-
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
 
 /**
  * Created by xueva on 3/10/2019.
  */
 
 public class CryptoSearch extends Fragment {
+    String crypto;
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         return inflater.inflate(R.layout.cryptosearch_layout, container, false);
+
+
     }
 }
